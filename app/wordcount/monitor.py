715aa5e174b8b4ecec98160b200abfe18df0d7ef
ZCQzhangyu <1098643527@qq.com>
2022-01-23 23:52:57 -0500

    changed perf_sender.py

--- app/wordcount/monitor.py
+++ app/wordcount/monitor.py
@@ -2,12 +2,14 @@ from src.monitor import Monitor
 from src.packetstream import PacketStream
 from config.config_hw import conf
 
+WORDCOUNT_QID = 1
+
 qconf = {
 	"split": 2,
 	"is_hash": True
 }
 
-queries = [PacketStream(0, 'WordCount', qconf)
+queries = [PacketStream(1, 'WordCount', qconf)
 			.Reduce('origin', 'sum', 3, 4096)
         	.Filter('origin', '32w3', '>=')
 			]
--- src/perf_sender.py
+++
@@ -1,6 +0,0 @@
-from src.sender_server import SenderServer
-
-from config.config_hw import sd_conf
-server = SenderServer(sd_conf)
-server.start()
-server.finish()
