e2060af04e35b70d69f250bb3ec0dab08427e028
dzm <infinityedge@pku.edu.cn>
2022-03-30 04:10:29 -0400

    test fix p4generator

---
+++ p4/test_generator/command.py
@@ -0,0 +1,26 @@
+
+bfrt.simple_l3.pipe.SwitchIngress.get_threshold.tbl_get_threshold.clear()
+bfrt.simple_l3.pipe.SwitchIngress.get_threshold.tbl_get_threshold.add_with_tbl_get_threshold_act(qid=0, threshold=10)
+bfrt.simple_l3.pipe.SwitchIngress.get_threshold.tbl_get_threshold.dump()
+
+
+bfrt.simple_l3.pipe.SwitchIngress.ipv4_lpm.clear()
+bfrt.simple_l3.pipe.SwitchIngress.ipv4_lpm.add_with_ipv4_forward(hdr_ipv4_dst_addr=ip("10.1.100.2"), hdr_ipv4_dst_addr_p_length=32, dst_addr=mac("ff:ff:ff:ff:ff:ff"), port=60)
+bfrt.simple_l3.pipe.SwitchIngress.ipv4_lpm.dump()
+
+
+import time
+
+flag = 0
+while True:
+	curtime = int(time.time())
+	if flag:
+		bfrt.simple_l3.pipe.SwitchIngress.stflag.clear()
+		flag = 0
+	else:
+		bfrt.simple_l3.pipe.SwitchIngress.stflag.add_with_flag1(flag = 0)
+		bfrt.simple_l3.pipe.SwitchIngress.stflag.add_with_flag1(flag = 1)
+		flag = 1
+	while True:
+		if curtime != int(time.time()):
+			break
